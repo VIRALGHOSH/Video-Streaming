09b1730866064140608e80995c5ed4261fafbe01
viralghosh <viralghosh@gmail.com>
2017-03-22 11:46:31 +0530

    payment controler

--- Video Streaming/Controllers/PaymentController.cs
+++ Video Streaming/Controllers/PaymentController.cs
@@ -24,18 +24,19 @@ namespace Video_Streaming.Controllers
         {
             return View(db.tbl_subscriptions.Where(x => x.sub_end_date > DateTime.Now).ToList());
         }
-        [CustomAuthorization_User(LoginPage = "~/Login/signin")]
+        //[CustomAuthorization_User(LoginPage = "~/Login/signin")]
        
         public ActionResult Paypal()
         {
-            tbl_subscription tb = new tbl_subscription();
-            tb.login_id = SessionData.UserId;
-            tb.sub_pay_date = DateTime.Now;
-            tb.sub_end_date = DateTime.Today.AddMonths(1);
-            tb.sub_amt = 5;
-            db.tbl_subscriptions.InsertOnSubmit(tb);
-            db.SubmitChanges();
-            return RedirectToAction("Index");
+            //tbl_subscription tb = new tbl_subscription();
+            //tb.login_id = SessionData.UserId;
+            //tb.sub_pay_date = DateTime.Now;
+            //tb.sub_end_date = DateTime.Today.AddMonths(1);
+            //tb.sub_amt = 5;
+            //db.tbl_subscriptions.InsertOnSubmit(tb);
+            //db.SubmitChanges();
+            //return RedirectToAction("Index");
+            return View();
         }
     }
 }
